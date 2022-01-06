bd2ec4991f3af0e066053d59b08507f61fbfaf66
Ahmed Ali <ahmedalimohamed1994@gmail.com>
2022-01-06 23:51:18 +0200

    Modularize project function

--- 1-node-farm/starter/node-farm.js
+++ 1-node-farm/starter/node-farm.js
@@ -21,19 +21,7 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-
     if (err) return console.log(err);
 });
 
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%productName%}/g, product.productName);
-    output = output.replace(/{%image%}/g, product.image);
-    output = output.replace(/{%price%}/g, product.price);
-    output = output.replace(/{%from%}/g, product.from);
-    output = output.replace(/{%nutrients%}/g, product.nutrients);
-    output = output.replace(/{%quantity%}/g, product.quantity);
-    output = output.replace(/{%description%}/g, product.description);
-    output = output.replace(/{%id%}/g, product.id);
-    if (!product.organic) output = output.replace(/{%not_organic%}/g, 'not-organic');
-    return output;
-
-}
+const replaceTemplate = require('./modules/replaceTemplate');
 
 //CREATE SERVER
 const server = http.createServer((req, res) => {
